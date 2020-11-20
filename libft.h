1ad00ba12964e25f4eadef0b8019f1b17c579605
Sheschire <theo.lemesle42@gmail.com>
2020-11-20 16:54:32 +0100

    changed libft.h char to int

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 13:19:34 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/20 11:26:40 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/20 16:54:09 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	ft_tolower(int c);
-char	ft_toupper(int c);
+int 	ft_tolower(int c);
+int 	ft_toupper(int c);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
