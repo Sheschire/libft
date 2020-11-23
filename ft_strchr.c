4cce2155f0aedcbbfd9f676ecedaecb445195688
Sheschire <theo.lemesle42@gmail.com>
2020-11-23 13:22:07 +0100

    qiuw

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 08:59:36 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/23 13:18:57 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/23 13:21:41 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 char	*ft_strchr(const char *s, int c)
 {
 	char	*str;
+	int		i;
 
+	i = 0;
 	str = (char *)s;
 	while (str)
 	{
-		if (*str == (unsigned char)c)
-			return (str);
-		if (*str == '\0')
-			return (str);
-		str++;
+		if (str[i] == (unsigned char)c)
+			return (str + i);
+		i++;
 	}
+	if (str[i] == '\0')
+			return (0);
 	return (0);
 }
