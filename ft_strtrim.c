b6f1ecb90f0e2535176c7a5ccf775f8821ea6a16
Sheschire <theo.lemesle42@gmail.com>
2020-11-23 15:35:08 +0100

    dewf

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,25 +6,11 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 21:54:05 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/23 15:06:10 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/23 15:31:25 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int		is_set(char c, char const *set)
-{
-	int		i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (c == set[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
 char			*ft_strtrim(char const *s1, char const *set)
 {
@@ -35,12 +21,12 @@ char			*ft_strtrim(char const *s1, char const *set)
 		return (0);
 	if (!set)
 		return (ft_strdup(s1));
-	while (*s1 && is_set(*s1, set))
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size = ft_strlen(s1);
-	while (size && is_set(s1[size], set))
+	while (size && ft_strchr(set, s1[size]))
 		size--;
-	trim = ft_substr((char *)s1, 0, size + 1);
+	trim = ft_substr(s1, 0, size + 1);
 	if (trim == NULL)
 		return (0);
 	return (trim);
