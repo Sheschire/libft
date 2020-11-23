18b1e4d8bfcdbc4390a15cf8b16f711210f89642
Sheschire <theo.lemesle42@gmail.com>
2020-11-23 14:01:07 +0100

    daw

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 18:26:36 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/23 13:51:21 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/23 13:59:30 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*ptr;
 
-	ptr = malloc(sizeof(size * nmemb));
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (0);
 	ft_memset(ptr, 0, nmemb * size);
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 19:11:01 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/23 13:56:05 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/23 14:01:03 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	sub = (char *)malloc(sizeof(char) * len + 1);
 	if (sub == NULL)
 		return (0);
