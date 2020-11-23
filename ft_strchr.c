13b5bfb94843f66d6105d127b45bcf72725b272d
Sheschire <theo.lemesle42@gmail.com>
2020-11-23 13:22:21 +0100

    fwe

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 08:59:36 by tlemesle          #+#    #+#             */
-/*   Updated: 2020/11/23 13:21:41 by tlemesle         ###   ########.fr       */
+/*   Updated: 2020/11/23 13:22:18 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ char	*ft_strchr(const char *s, int c)
 		i++;
 	}
 	if (str[i] == '\0')
-			return (0);
+			return (str + i);
 	return (0);
 }
